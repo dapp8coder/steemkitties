aa913aef180981a5afb973cd0889d6f9209a984a
sdavignon <sdavignon1@gmail.com>
2018-02-21 16:40:32 -0500

    testing

--- assets/cryptokitties.js
+++ assets/cryptokitties.js
@@ -42,7 +42,7 @@ var writeHTML = "";
 
 function adventureForm(){
 	$('.Editor0').html('<img src="/images/wait.gif" />');
-	$('.Editor').hide();
+	
 	$('.Editor0').show();
 		
 	var jsonURL = "/backgrounds.json?v=1";
@@ -64,14 +64,14 @@ function adventureForm(){
 	$('.Editor0').html('<div id="cryptokitties" class="ant-row ant-form-item" class="height:200px;"></div><div id="backgrounds" class="ant-row ant-form-item" class="height:200px;">'+imgList+'</div><div class="ant-row ant-form-item"><img id="img_canvas" src="/images/backgrounds/bg1.png" width="800" height="500" /></div><div class="Editor__bottom"><div class="Editor__bottom__right"><div class="ant-row ant-form-item"><div class="ant-form-item-control-wrapper"><div class="ant-form-item-control "><button class="" type="">Meow Next</button></div></div></div></div></div>'); 
 
 	renderUserCryptokitties(user);	
-	tid = setTimeout(attachEventSelectors, 2000);
+	tid = setTimeout(attachEventSelectors, 1000);
 }
 
 
 $(document).ready(function() {
+	$('.Editor').hide();
 	
-	
-	var tid0 = setTimeout(adventureForm, 1000);
+	var tid0 = setTimeout(adventureForm, 500);
  
 	//attachEventSelectors();
 	
